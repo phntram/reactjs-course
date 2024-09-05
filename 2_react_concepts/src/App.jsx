3f3669f5f268f05db981c2d65b743ca4888958e1
phntram <nguyettram10@gmail.com>
2024-09-05 09:02:01 +0700

    2_react_concepts: class component

--- 2_react_concepts/src/App.jsx
+++ 2_react_concepts/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import ClassBasedComponent from './components/class-based-component';
 
 function App() {
 
 	return (
 		<>
 			<h1>ReactJs Course 2024</h1>
+			<ClassBasedComponent></ClassBasedComponent>
 		</>
 	);
 }
---
+++ 2_react_concepts/src/components/class-based-component.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+// don't use this anymore, they try to migrating the class component to function component.
+class ClassBasedComponent extends Component {
+    // render method to render a jsx code.
+    render() {
+        return (
+            <div>
+                <h3>Class Based Component</h3>
+            </div>
+        );
+    }
+}
+
+
+export default ClassBasedComponent;
